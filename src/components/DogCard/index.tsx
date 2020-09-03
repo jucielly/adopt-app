34f09f49b2df79f7ee201fe63c936cf87d30e356
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-09-02 22:09:39 -0300

    style update

--- src/components/DogCard/index.tsx
+++ src/components/DogCard/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
 import Logo from '../../assets/logo.svg';
 import './style.css';
-import { type } from 'os';
+import { IMAGE_API_URL } from "../../consts/api"
 
 export type Dog = {
+  id: number
   name: string
   breed: string
   subBreed: string
@@ -18,15 +20,24 @@ type DogCardProps = {
 
 const DogCard: React.FC<DogCardProps> = ({ dog }) => {
   const {
-    name, breed, subBreed, gender, age,
+    name, breed, subBreed, gender, age,id
   } = dog;
+
+  const [image, setImage] = useState<string>()
+
+  useEffect(() => {
+    setImage(undefined)
+    axios(IMAGE_API_URL.replace(":breed", breed))
+      .then(({ data }) => setImage(data.message))
+  }, [id])
+
   return (
 
     <div className="dog-card">
       <h3 className="dog-card-title">{name}</h3>
-      <img src={Logo} alt="doggo" className="card-logo" />
+      <img src={image || Logo} alt="doggo" className="card-logo" />
       <p className="dog-card-info">{breed}</p>
-      <p className="dog-card-info">{subBreed}</p>
+      <p className="dog-card-info">{subBreed || "não há sub-raça"}</p>
       <p className="dog-card-info">{gender}</p>
       <p className="dog-card-info">{age} anos</p>
     </div>
