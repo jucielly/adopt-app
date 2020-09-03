35aba2a2325115a7ffda3b974034a56348484f00
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-09-02 22:09:51 -0300

    fetch apis

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react'
+import axios from "axios"
 import './assets/reset.css';
-import './assets/app.css';
+import './assets/app.css'
 import Navbar from './components/NavBar';
-import SearchForm from './components/SearchForm';
+import SearchForm, { FormValues } from './components/SearchForm';
 import DogList from "./components/DogList"
+import { Dog } from "./components/DogCard"
+import { API_URL } from "./consts/api"
 
-function App() {
+
+const App: React.FC = () => {
+
+  const [dogs, setDogs] = useState<Dog[]>([])
+
+  const handleSearch = (values: FormValues) => {
+    axios(API_URL, {
+      params: {
+        ...values,
+        gender: values.gender === "Fêmea" ? "female" : values.gender === "Macho" ? "male" : undefined
+      }
+    }).then(({ data }) => setDogs(data))
+  }
+
+
   return (
     <>
       <Navbar />
       <main>
-        <SearchForm />
-        <DogList dogs={[{
-          name:"julia",
-          breed: "lhasa",
-          subBreed: "apso",
-          gender: "female",
-          age: 1
-        },
-      ]}/>
+        <SearchForm onSearch={handleSearch} />
+        <DogList dogs={dogs} />
       </main>
 
     </>
