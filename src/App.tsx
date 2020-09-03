322cdbe14616fd7836a792d06acb91e6d64622ce
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-09-03 17:39:34 -0300

    loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,41 @@ import Navbar from './components/NavBar';
 import SearchForm, { FormValues } from './components/SearchForm';
 import DogList from "./components/DogList"
 import ErrorCard from './components/ErrorCard'
+import LoadingSpinner from "./components/Loadingspinner"
 import { Dog } from "./components/DogCard"
 import { API_URL } from "./consts/api"
 
 
 const App: React.FC = () => {
-
   const [dogs, setDogs] = useState<Dog[]>([])
-
+  const [load, setLoad] = useState(false)
+  const [error, setError] = useState<string>()
   const [hasSearch, setHasSearch] = useState(false)
 
   const handleSearch = (values: FormValues) => {
-    setHasSearch(true)
+    setLoad(true)
     axios(API_URL, {
       params: {
         ...values,
         gender: values.gender === "Fêmea" ? "female" : values.gender === "Macho" ? "male" : undefined
       }
-    }).then(({ data }) => setDogs(data))
+    }).then(({ data }) => {
+      setError(undefined)
+      setHasSearch(true)
+      setDogs(data)
+    })
+      .catch((error) => {
+        if (error.response) {
+          setError("Ocorreu um erro")
+        } else if (error.request) {
+          setError("Verifique sua internet")
+        } else {
+          setError("Ocorreu algo inesperado")
+        }
+      })
+      .finally(() => {
+        setLoad(false)
+      })
   }
 
 
@@ -32,7 +49,8 @@ const App: React.FC = () => {
       <Navbar />
       <main>
         <SearchForm onSearch={handleSearch} />
-        {dogs.length < 1 && hasSearch && <ErrorCard message="doggo não encontrado :c"/>}
+        {load && <LoadingSpinner />}
+        {dogs.length < 1 && hasSearch && <ErrorCard message={error || "doggo não encontrado :c"} />}
         <DogList dogs={dogs} />
       </main>
 
