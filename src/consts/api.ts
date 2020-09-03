a46a7aa97c486b31fcdd7f223d7855d45da2ddd8
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-09-03 20:49:30 -0300

    new api url

--- src/consts/api.ts
+++ src/consts/api.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://192.168.0.13:3333/dogs";
+export const API_URL = "https://adoteumamigo.jucielly.dev/api/dogs";
 export const IMAGE_API_URL = "https://dog.ceo/api/breed/:breed/images/random"
